9d32d7a9c978e8b9b209403f3fb22e0e737f3bfc
Dan846 <danilam846@gmail.com>
2023-04-12 01:59:48 +0700

    uploads

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
+import Cards from './components/Cards'
+import Form from './components/Form'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [cards, setCards] = useState([]);
+	const [form, setForm] = useState({
+		content: ''
+	});
+
+	const loadCards = () => {
+		fetch('http://localhost:7070/notes')
+			.then(response => response.json())
+			.then(data => setCards(data));
+	};
+
+	useEffect(loadCards, []);
+
+	return (
+		<div className="App">
+			<Cards cards={cards} setCards={setCards} loadCards={loadCards} />
+			<Form form={form} setForm={setForm} cards={cards} setCards={setCards} loadCards={loadCards} />
+		</div>
+	)
 }
 
-export default App;
+export default App
